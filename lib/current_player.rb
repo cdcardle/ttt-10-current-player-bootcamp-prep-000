a4bb75ecf9ac986b0a6ada3270a97ef7187393c6
Chris Cardle <cdcardle@gmail.com>
2017-10-05 17:01:59 +0000

    automatically backed up by learn

---
+++ lib/current_player.rb
@@ -0,0 +1,16 @@
+def turn_count(board)
+  count = 0
+    board.each do |position|
+      if (position == " ")
+        false
+      else
+        count += 1
+      end
+      puts count
+    end
+end
+
+
+def current_player
+end
+board = [" ", " ", " ", " ", " ", " ", "X", "O", "X"]
